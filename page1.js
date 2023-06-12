c129424c269b53f2cfaff013b588a5f50a40cfff
hanachou <hanachou20061101@gmail.com>
2023-06-12 12:47:30 +0800

    js

---
+++ page1.js
@@ -0,0 +1,7 @@
+$("#ecolor").mouseenter(
+    function(){
+        $(".event").css("transition-duration","0.1s");
+        $(".event").css("transition-timing-function","ease-in-out");
+        $(".event").css("color","#d5e3d4");
+    }
+);
