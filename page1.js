d77b389a2e6801bb914ef50651734e3ec40bf8d0
hanachou <hanachou20061101@gmail.com>
2023-06-12 13:08:43 +0800

    delete

--- page1.js
+++
@@ -1,7 +0,0 @@
-$("#ecolor").mouseenter(
-    function(){
-        $(".event").css("transition-duration","0.1s");
-        $(".event").css("transition-timing-function","ease-in-out");
-        $(".event").css("color","#d5e3d4");
-    }
-);
